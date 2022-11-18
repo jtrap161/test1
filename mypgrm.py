011916f48165f121e4820d687237d0b66322daca
Joseph Trapani <trapani0007@aol.com>
2022-11-18 17:21:50 -0500

    added initial code

--- mypgrm.py
+++ mypgrm.py
@@ -1,1 +1,14 @@
-print("hello world")
+#this program will sort strings provided by user, alphabetically
+my_str = input('Enter a string: ')
+
+#breakdown string into list of words
+words = [word.lower() for word in my_str.split()]
+
+#sort list
+words.sort()
+
+#display th sorted words
+print("The sorted words are: ")
+
+for words in words:
+    print(words)
